9779717fac7d80e56d1b63ae1a55fe8f3c93d0ac
Fady Ibrahim <fadyibrahim37@gmail.com>
2024-05-17 09:11:24 -0400

    code works but not clean, currently uses two loops one to set the has map and one to see if difference (target - elm)

--- Java/1_two_sum.java
+++ Java/1_two_sum.java
@@ -8,4 +8,21 @@ class Solution {
     		hmap.put(elm,x);
     		x++;
     	}
+        for (int i=0; i<nums.length;i++)
+        {
+            Integer numtarg=target-nums[i];
+            System.out.println(numtarg);
+            //int index=hmap.get(numtarg);
+            if(hmap.get(numtarg) != null)
+            {
+                if (hmap.containsKey(numtarg) && i!=hmap.get(numtarg))
+                {
+                    int[] randomArray = {i,hmap.get(numtarg)};
+                    return randomArray;
+                }
+            }
+        }
+        int[] randomArray = {0,0};
+        return randomArray; 
+    }
 }
