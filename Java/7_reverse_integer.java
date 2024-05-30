290349d1298d7ee421cf0d13724266082d54c6dd
Fady Ibrahim <fadyibrahim37@gmail.com>
2024-05-30 10:24:17 -0400

    algo using math to find reverse by shifting by base 10

--- Java/7_reverse_integer.java
+++ Java/7_reverse_integer.java
@@ -1,22 +1,13 @@
 class Solution {
-    public static int reverse(int x) {
-        String reversednumber = String.valueOf(x);
-        long reversenum=0;
-        String hold=new StringBuilder(reversednumber).reverse().toString();
-        int ispositive=1;
-        if (x< 0)
-        {
-            ispositive=-1;
-            reversenum = Long.parseLong(hold.substring(0,hold.length()-1));
-        }    
-        else{
-            reversenum = Long.parseLong(hold);   
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
         }
-
-        if (reversenum> Integer.MAX_VALUE || reversenum< Integer.MIN_VALUE)
-        {
-            return 0;
-        }
-        return ispositive * (int)reversenum;
+        return rev;
     }
 }
