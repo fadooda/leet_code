9c1b9ba6b567266ab1cb72b0733094579cd6e267
Fady Ibrahim <fadyibrahim37@gmail.com>
2024-06-03 09:41:05 -0400

    finding palindrome of integer algo using math base 10 O(logn)

--- Java/9_palindrome_number.java
+++ Java/9_palindrome_number.java
@@ -1,16 +1,23 @@
-import java.util.*; 
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0)
-        {
+        // Special cases:
+        // As discussed above, when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-        String orig = Integer.toString(x);
-        String palstr= new StringBuilder(orig).reverse().toString();
-        if(orig.equals(palstr))
-        {
-            return true;
+
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
         }
-        return false;
+
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+        return x == revertedNumber || x == revertedNumber/10;
     }
 }
