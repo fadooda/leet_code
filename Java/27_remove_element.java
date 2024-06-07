326f0a0c594514c604ff87689af9435116a9b202
Fady Ibrahim <fadyibrahim37@gmail.com>
2024-06-07 07:23:41 -0400

    efficent... using two pointer algo. also added coments.

--- Java/27_remove_element.java
+++ Java/27_remove_element.java
@@ -1,29 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int length=nums.length;
-        for (int i=0; i<length;i++)
-        {
-        //System.out.println(nums[i]);   
-            if (nums[i]==val)
-            {   
-                //System.out.println(nums[i]);
-                length--;
-          //System.out.println("at index " + i + " is " + nums[i]);  
-                if (length > 0 )
-                {
-                    System.out.println(length);
-                    while( length > 0 && i !=length && nums[length]==val )
-                    {
-                        //System.out.println(length);
-                        length--;
-                    }
-                    nums[i]=nums[length];
-                    nums[length]= val; 
-                }
-                     //System.out.println("at index " + i + " is " + nums[i]);      
+        int index = 0; // Slow pointer to place valid elements
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i]; // Overwrite unwanted values
+                index++;
             }
-        //System.out.println("at index " + i + " is " + nums[i]);            
         }
-        return length;
+        
+        return index; // The new length of the array without `val`
     }
 }
