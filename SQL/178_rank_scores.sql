645bba73b344f71d97337492a1609a4914eea147
Fady Ibrahim <fadyibrahim37@gmail.com>
2024-07-17 09:00:00 -0400

    ranking scores using dense rank.

--- SQL/178_rank_scores.sql
+++ SQL/178_rank_scores.sql
@@ -1,7 +1,5 @@
 /* Rank scores*/ 
-Select sboard.Score,
-       (Select Count(1) + 1  /* add one to the counter to begin at 1 instead of zero  */
-	   From (Select Distinct Score from Scores) as uniqScore 
-	   where sboard.Score <uniqScore.Score) as "Rank"  /*only increment when the sboard is less than uniqScore*/
-From Scores sboard 
-Order by sboard.Score Desc;
+Select Score,
+       dense_rank() over (order by score desc) as "Rank"  /*make a rank based on score desc*/
+From Scores 
+
