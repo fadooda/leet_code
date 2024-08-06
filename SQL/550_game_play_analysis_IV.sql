8e9564061dd2772a42d98ae4a1b8e723cdf51311
Fady Ibrahim <fadyibrahim37@gmail.com>
2024-08-06 08:12:39 -0400

    updated query to use first_value window function instead for efficiency

--- SQL/550_game_play_analysis_IV.sql
+++ SQL/550_game_play_analysis_IV.sql
@@ -2,12 +2,10 @@ WITH cte AS (
     SELECT 
         a.player_id,
         a.event_date,
-        -- Find the first event date for each player using a correlated subquery (slow!)
-        (SELECT MIN(b.event_date) FROM Activity b WHERE b.player_id = a.player_id) AS first_event_date,
         -- Check if this login happened exactly one day after the first login
         CASE 
             WHEN DATEDIFF(a.event_date, 
-                (SELECT MIN(b.event_date) FROM Activity b WHERE b.player_id = a.player_id)) = 1 
+                FIRST_VALUE(a.event_date) OVER (PARTITION BY a.player_id ORDER BY a.event_date)) = 1 
             THEN 1 
             ELSE 0 
         END AS userLoggedInNextDay
